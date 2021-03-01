3287521e02e17a2405e4380e600fca892fb47908
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-01 17:17:19 -0500

    Fixes moving logic to the params controller from the application controller (wrong) and formats output json data correctly

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,2 +1,8 @@
 class Api::ParamsController < ApplicationController
+  def input_param
+    input_message = params[:cap]
+    # params["cap"] or params[:cap], enter http://localhost:3000/api/params?cap=helloworld
+    @output = input_message.upcase
+    render "params.json.jb"
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: -> { request.format.html? }
-
-  def input_param
-    input_message = params[:cap]
-    # params["cap"] or params[:cap], enter http://localhost:3000/api/params?cap=helloworld
-    @output = input_message.upcase
-    render "params.json.jb"
-  end
 end
